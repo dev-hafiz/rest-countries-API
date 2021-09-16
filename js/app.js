0fac57d12e454691c8625f919f21d506a8b4dadb
okshi <johnstarhafiz@gmail.com>
2021-09-17 01:22:09 +0600

    This commit need for object destructring

--- js/app.js
+++ js/app.js
@@ -7,11 +7,10 @@ const loadRestDaat = () => {
 //Call This Function
 loadRestDaat()
 
-
 // UI show DATA 
 const showInUiDisplay = countries => {
+     console.log(countries);
      const countriesParant = document.getElementById('countries');
-
      const UiUpdate = countries.map(country => makeHTMlComponant(country));
      countriesParant.innerHTML = UiUpdate.join(' ');
 }
@@ -19,14 +18,20 @@ const showInUiDisplay = countries => {
 
 // Makes componant 
 const makeHTMlComponant = country => {
+     const {
+          name,
+          capital,
+          population,
+          borders,
+          flag
+     } = country;
      return `
      <div class = "country-style">
-     
-     <h2>${country.name}</h2>
-     <h4>${country.capital}</h4>
-     <h5>${country.population}</h5>
-     <h5>${country.borders}</h5>
-     <img src = "${country.flag}">
+     <h2>Country : ${name}</h2>
+     <h4>City Name : ${capital}</h4>
+     <h5>Population : ${population}</h5>
+     <h5>Country Borders : ${borders}</h5>
+     <img src = "${flag}">
      </div>
 
       `
